bd058e133d6a0dfdd8bfd6da5be3ba529fb4ef75
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2025-01-07 02:06:47 +0530

    cart product view

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,42 @@
+import{ React,useState} from 'react'
+import SummerApi from '../common'
+
+function Cart() {
+    const [data,setData] = useState([])
+    const [loading,setLoading] = useState(false)
+
+    const fetchData=async()=>{
+        setLoading(true)
+        const response=await fetch(SummerApi.addtoCartProductView.url,{
+            method:SummerApi.addtoCartProductView.method,
+            headers:{
+                'Content-Type': 'application/json',
+            },
+
+        })
+        const responseData = await response.json()
+
+        if(responseData.success){
+            setData(responseData.data)
+        }
+    }
+  return (
+
+    <div className='mx-auto container'>
+    <div className=' text-center text-lg py-3'>
+        {
+            data.length===0 & !loading && (
+                <div>no data</div>
+            )
+               
+            
+        }
+    </div>
+    <div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Cart
