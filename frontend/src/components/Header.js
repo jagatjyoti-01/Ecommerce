967ad6b79660bdb7e18522b72704ff663bab1ae4
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2025-01-06 14:53:43 +0530

    jj

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -42,7 +42,7 @@ const Header = () => {
       <div className="h-full container mx-auto flex items-center px-4 justify-between">
         <div className=''>
           <Link to={"/"}>
-            <Logo w={100} h={60} />
+            <Logo w={100} h={62} />
           </Link>
         </div>
 
